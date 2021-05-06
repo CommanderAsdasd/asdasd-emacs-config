74695a3a025fdb01bc2b8ee38a153ee817859dbc
Your Name <you@example.com>
2021-05-06 20:55:44 +0200

    add init-windmove

--- init.el
+++ init.el
@@ -13,6 +13,7 @@
 
 (require 'asdasd-unstable)
 (require 'init-helm)
+(require 'init-windmove)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
