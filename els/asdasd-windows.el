ab55f0e901a50253897b2ede57b31e7c93b7da14
Your Name <you@example.com>
2021-07-09 13:48:52 +0200

    [WIP] rusification on windows

--- els/asdasd-windows.el
+++ els/asdasd-windows.el
@@ -16,6 +16,35 @@
     (call-interactively 'shell))
   )
 
+;; rusification
+
+;; ”станавливаем €зыковую среду
+(set-language-environment 'Cyrillic-CP)
+;; ”станавливаем кодировки по умолчанию дл€ файлов, буферов и обмена с внешними программами
+(setq default-buffer-file-coding-system 'koi8-r)
+(prefer-coding-system 'koi8-r)
+(define-coding-system-alias 'koi8-u 'koi8-r)
+(set-terminal-coding-system 'koi8-r)
+(set-keyboard-coding-system 'koi8-r)
+
+;; следующие две строки использовать с осторожностью:
+;; не рекомендую глобально мен€ть значени€ coding-system-for-read & coding-system-for-write,
+;; например, это может отразитьс€ на кодировке при общении по сети. 
+;; эти переменные обычно используютс€ только дл€ конкретных I/O операций.
+;; см. документацию и bug #28780
+(setq-default coding-system-for-read 'koi8-r)
+(setq-default coding-system-for-write 'koi8-r)
+
+(setq selection-coding-system 'koi8-r)
+(setq default-process-coding-system
+   (if (string> (substring emacs-version 0 2) "21") '(koi8-r . koi8-r) 'koi8-r))
+(put-charset-property 'cyrillic-iso8859-5 'preferred-coding-system 'koi8-r)
+;; ѕоддержка русской кодировки MS Windows
+(codepage-setup 1251)  
+(define-coding-system-alias 'windows-1251 'cp1251)
+(define-coding-system-alias 'win-1251 'cp1251)
+(set-input-mode nil nil 'We-will-use-eighth-bit-of-input-byte)
+
 
 
 ;; (defun )
