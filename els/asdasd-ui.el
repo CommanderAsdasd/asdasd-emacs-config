bcbaa56692208305b9dffd39fed5a46712abc457
unknown <e1ectricwiz4rd@gmail.com>
2022-11-08 15:47:56 +0100

    asdasd-ui

---
+++ els/asdasd-ui.el
@@ -0,0 +1,13 @@
+(use-package vscode-dark-plus-theme
+  :config
+  (load-theme 'vscode-dark-plus t)
+  (set-frame-font "-outline-Source Code Pro-bold-normal-normal-mono-17-*-*-*-c-*-iso8859-7" nil t))
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+
