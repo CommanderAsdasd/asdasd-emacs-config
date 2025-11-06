c42b5cc9f53d2de801d1528225346e7d650a71d8
asdasd <e1ectricwiz4rd@gmail.com>
2025-11-06 17:49:51 +0100

    clipboard exchanger

--- els/asdasd-os-clipboard.el
+++ els/asdasd-os-clipboard.el
@@ -1,5 +1,11 @@
+(defun asdasd-os-clipboard-howm-clipboard-exchanger ()
+  "workaround for a broken clipboard"
+  (write-region (car kill-ring) nil (expand-file-name "clipboar" howm-directory) 'append))
+
 (use-package clipmon
   :bind* ("M-s c m" . clipmon-autoinsert-toggle)
-  :config (clipmon-mode))
+  :config (clipmon-mode)
+  (add-hook 'clipmon-mode-hook ()))
 
 (use-package ox-clip)
+
