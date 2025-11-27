f7a8d203aca3b64f4fbb93dd6846b5ba5101cecb
asdasd <e1ectricwiz4rd@gmail.com>
2025-11-27 15:48:24 +0100

    [2025-11-27 15:48] wsl push wrapper function

--- els/asdasd-sync.el
+++ els/asdasd-sync.el
@@ -4,5 +4,9 @@
   (interactive)
   (async-shell-command "rsync -ravz --checksum -av \"/ctxmnt/C5405944@GLOBAL.CORP.SAP/default/C/Users/AleksandrTankovskii(/.emacs.d/\" ~/.emacs.d/"))
 
+(defun asdasd-sync-wsl-emacs-d-push ()
+  (interactive)
+  (async-shell-command "wsl -e bash -c \"cd /home/leksandrankovskii/.emacs.d; git push\"" ))
+
 
 (provide 'asdasd-sync)
